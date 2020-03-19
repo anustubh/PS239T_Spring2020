d1a3e8ce6162bda775f7519a96ad1dce0869c157
Julia Christensen <jbchristensen@berkeley.edu>
2020-03-18 18:11:22 -0700

    updated vis file structure

---
+++ 09_r-analysis-visualization/06_setup/api_key.R
@@ -0,0 +1,9 @@
+###
+### INSTRUCTIONS
+###
+
+# 1) Sign up for census api key using this link: https://api.census.gov/data/key_signup.html
+# 2) Follow instructions and get your own api key 
+# 3) Copy and paste API key below (replacing Julia's API key)
+
+census_api_key <- "2ca10aee6d7740f534d0ab3bf32badeafa4eeb72" #replace with your own API key
